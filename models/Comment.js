c2ec7660c956c0c345173bf58f731d38ae372663
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-20 09:11:50 -0700

    update model files

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
 
 class Comment extends Model {}
 
@@ -7,38 +7,38 @@ Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     comment_text: {
       type: DataTypes.STRING,
-      allowNull: false,
       validate: {
-        len: [1]
-      }
+        len: [3],
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'user',
-        key: 'id'
-      }
+        key: 'id',
+      },
     },
     post_id: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'post',
-        key: 'id'
-      }
-    }
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment'
+    modelName: 'comment',
   }
-);
+)
 
-module.exports = Comment;
+module.exports = Comment
--- models/index.js
+++ models/index.js
@@ -1,69 +1,34 @@
-// import all models
-const Post = require('./Post');
-const User = require('./User');
-const Vote = require('./Vote');
-const Comment = require('./Comment');
+const User = require('./User')
+const Post = require('./Post')
+const Comment = require('./Comment')
 
-// create associations
 User.hasMany(Post, {
-  foreignKey: 'user_id'
-});
+  foreignKey: 'user_id',
+})
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
-
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
+  onDelete: 'cascade',
+})
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
+  onDelete: 'cascade',
+})
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
+  onDelete: 'cascade',
+})
 
 User.hasMany(Comment, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
+  onDelete: 'cascade',
+})
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id'
-});
+  foreignKey: 'post_id',
+  onDelete: 'cascade',
+})
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment }

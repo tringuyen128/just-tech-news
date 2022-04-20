71eeac402ed5d723a30f76f058d1e7ab082f3f8b
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-20 08:36:53 -0700

    update seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,22 @@
-const seedUsers = require('./user-seeds');
-const seedPosts = require('./post-seeds');
-const seedComments = require('./comment-seeds');
-const seedVotes = require('./vote-seeds');
+const seedUsers = require('./user-seeds')
+const seedPosts = require('./post-seeds')
+const seedComments = require('./comment-seeds')
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection')
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('--------------');
-  await seedUsers();
-  console.log('--------------');
+  await sequelize.sync({ force: true })
+  console.log('------DATABASE SYNCED--------')
+  await seedUsers()
+  console.log('--------USERS SEEDED------')
 
-  await seedPosts();
-  console.log('--------------');
+  await seedPosts()
+  console.log('-------POSTS SEEDED-------')
 
-  await seedComments();
-  console.log('--------------');
+  await seedComments()
+  console.log('-------COMMENTS SEEDED-------')
 
-  await seedVotes();
-  console.log('--------------');
+  process.exit(0)
+}
 
-  process.exit(0);
-};
-
-seedAll();
+seedAll()

9766f3fa751a42292f17800d05c3227b74f875aa
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-20 09:29:47 -0700

    complete and clean up

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
 
-require('dotenv').config();
+require('dotenv').config()
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    });
+      port: 3306,
+    })
 
-module.exports = sequelize;
+module.exports = sequelize

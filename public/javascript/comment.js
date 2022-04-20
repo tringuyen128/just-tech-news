e54518642e3aa7780ffa148d6d456950e69f03e3
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-20 08:40:31 -0700

    update public files

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,29 +1,36 @@
 async function commentFormHandler(event) {
-  event.preventDefault();
+  event.preventDefault()
 
-  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  const comment_text = document
+    .querySelector('input[name="comment-body"]')
+    .value.trim()
+
   const post_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
-  ];
+  ]
 
+  // if there is a comment -- preventing from users submitting empty comments
   if (comment_text) {
     const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({
         post_id,
-        comment_text
+        comment_text,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+        'Content-Type': 'application/json',
+      },
+    })
 
     if (response.ok) {
-      document.location.reload();
+      document.location.reload()
     } else {
-      alert(response.statusText);
+      alert(response.statusText)
+      document.querySelector('#comment-form').style.display = 'block'
     }
   }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', commentFormHandler)

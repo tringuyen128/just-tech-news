46a9d09f1817ac570658be8e171f1907bedd0842
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-20 20:09:44 -0700

    update server

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-const path = require('path')
 const express = require('express')
 const routes = require('./controllers')
 const sequelize = require('./config/connection')
-
-//helper function
+const path = require('path')
 const helpers = require('./utils/helpers')
-
-//session used to connect sequelize database
-const session = require('express-session')
-
-//express handlebars
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({ helpers })
-
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 3001
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
-
 const sess = {
   secret: 'Super secret secret',
   cookie: { maxAge: 36000 },
@@ -27,18 +18,13 @@ const sess = {
     db: sequelize,
   }),
 }
-
 app.use(session(sess))
-
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
-
 app.use(routes)
-
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'))
 })
